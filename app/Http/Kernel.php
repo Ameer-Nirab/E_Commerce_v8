f15ebe2c9dd75b77f35f62a86b6e710b6e9bd44b
Ameer-Nirab <ameer.nirab@gmail.com>
2023-08-26 20:50:51 +0300

    lec 21 Authentication

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\ChekUser;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -40,7 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
@@ -53,21 +54,22 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
-        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-        'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-        'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
-        'signed' => \App\Http\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'auth'                    => \App\Http\Middleware\Authenticate::class,
+        'auth.basic'              => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.session'            => \Illuminate\Session\Middleware\AuthenticateSession::class,
+        'cache.headers'           => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can'                     => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest'                   => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'password.confirm'        => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'precognitive'            => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
+        'signed'                  => \App\Http\Middleware\ValidateSignature::class,
+        'throttle'                => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified'                => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'localize'                => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
         'localizationRedirect'    => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
         'localeSessionRedirect'   => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
         'localeCookieRedirect'    => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
-        'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class
+        'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
+        'user_type'               => \App\Http\Middleware\CheckUser::class
     ];
 }
